f0be460773c14413cf86a4c351acfa3a968801d0
Cesar Silva <silvacde@gmail.com>
2019-10-29 12:46:04 -0700

    set up the models routes

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require('axios');
 var router = express.Router();
+
+var Note = require("../models/Note.js");
+var Article = require("../models/Article.js");
+
+router.get("/", function(req, res) {
+    res.render("index");
+});
