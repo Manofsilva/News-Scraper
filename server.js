7d71fe99fc56dbeef77b68665fecf81014847b65
Cesar Silva <silvacde@gmail.com>
2019-10-29 12:39:22 -0700

    created Note and Article file in Models folder and also setup to use express.router

---
+++ server.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var cheerio = require("cheerio");
+var axios = require('axios');
+var router = express.Router();
